f9db2638844ac70b951cfa5bdc19bb5216ec2982
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-03-10 14:49:59 +0200

    Added phase 1 and 2 and particle system plus ui icons

---
+++ Assets/_Vinyl Factory/Scripts/RotateVinyl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateVinyl : MonoBehaviour
+{
+
+    [SerializeField] private GameObject objectToRotate;
+    [SerializeField] private float rotateSpeed;
+
+    private void Update()
+    {
+        RotateObject();
+    }
+
+    private void RotateObject()
+    {
+      objectToRotate.transform.Rotate(Vector3.up * (rotateSpeed * Time.deltaTime));  
+    }
+}
