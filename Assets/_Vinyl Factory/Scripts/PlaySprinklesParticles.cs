ab98204d8c5812ec8b066092a194e169c5e9a11a
EdenAlon73 <61311576+EdenAlon73@users.noreply.github.com>
2021-03-14 11:16:36 +0200

    Started Animation

---
+++ Assets/_Vinyl Factory/Scripts/PlaySprinklesParticles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySprinklesParticles : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem sprinklesParticles;
+    [SerializeField] private bool playParticle;
+    private float yValue;
+
+    private void Awake()
+    {
+        yValue = transform.position.y;
+        sprinklesParticles.Stop();
+    }
+
+    private void Update()
+    {
+        SetParticleBool();
+        PlayParticle();
+    }
+
+    private void SetParticleBool()
+    {
+        yValue = transform.position.y;
+        print(yValue);
+        
+        if (yValue >= 7)
+        {
+            playParticle = false;
+        }
+        else
+        {
+            playParticle = true;
+        }
+    }
+
+    private void PlayParticle()
+    {
+        if (playParticle)
+        {
+            sprinklesParticles.Play();
+        }
+        else
+        {
+            sprinklesParticles.Stop();
+        }
+
+    }
+}
